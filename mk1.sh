a4137a17212c609895ff97ad73f55cbcd7d1dafa
Charlie <root@pbrian_laptop.camden.samlearning.local>
2009-11-07 21:23:46 +0000

    changed mk1 for testing

--- mk1.sh
+++ mk1.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+
+#####
+# pbrian paul@mikadosoftware.com
+# 
+# This is designed to execute on the remote repository running on live 
+# webserver, then build the pages and build my website.
+# but it does not run from the bare repo, but from the clone
+# clone : /home/pbrian/clone_upstream/cookbook/mk1.sh
+#####
+
+
 
 git pull
 python mkbook.py 
