61e2d0396cd36aaf694d7b07554ee4506e572269
Paul Brian <paul@mikadosoftware.com>
2010-09-08 08:48:15 +0100

    migrated to meta data stored in cookbook

--- testscript.py
+++ testscript.py
@@ -1,11 +1,16 @@
 
+import sys, os
+content_meta_dir = sys.argv[1:][0]
+
+sys.path.insert(0, os.path.abspath(content_meta_dir))
+
 
 import lib
 import mkbook
 import config
-chp_dir = '/home/pbrian/downloads/thebook/'
+chp_dir = '/home/pbrian/mikadosoftware.com/projects/cookbook'
 config.setup_chp_dir(chp_dir)
-p = lib.rst_to_page('/home/pbrian/downloads/thebook/SoHoFromScratch/DNS.chp')
+p = lib.rst_to_page(os.path.join(chp_dir, 'SoHoFromScratch/DNS.chp'))
 
 dirlist = mkbook.run_dirs()
 
