347050a0754768dc9fb401d540c01d317924e01e
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-05-03 20:46:39 +0100

    Convert to setup-style package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+version = '0.1.0'
+
+setup(name='bookmaker',
+      version=version,
+      author="paul brian",
+      author_email="paul@mikadosoftware.com"
+      )
